64cf34d3bd46bfe5da17cb0d7bb8a4c8b431f7ce
Louis DUCRUET <louis.ducruet@epsi.fr>
2025-06-08 20:40:19 +0000

    add astro template

---
+++ astro/idx-template.nix
@@ -0,0 +1,27 @@
+{pkgs, template ? "basics", version ? "latest", packageManager ? "npm", typescript ? "strict", git ? true, tailwind ? false, ... }: {
+  packages = [
+    pkgs.nodejs_22
+    pkgs.yarn
+    pkgs.nodePackages.pnpm
+    pkgs.bun
+    pkgs.j2cli
+    pkgs.nixfmt
+  ];
+
+  bootstrap = ''
+    mkdir "$out"
+    ${
+      if packageManager == "npm" then "npm create astro@${version} \"$out\" -- --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install"
+      else if packageManager == "yarn" then "yarn create astro \"$out\" --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install" 
+      else if packageManager == "pnpm" then "pnpm create astro \"$out\" --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install"
+      else if packageManager == "bun" then "bun create astro \"$out\" --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install"
+      else "npm create astro@${version} \"$out\" -- --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install"
+    }
+
+    mkdir -p "$out"/.idx
+    packageManager=${packageManager} tailwind=${if tailwind then "true" else "false"} j2 ${./devNix.j2} -o "$out"/.idx/dev.nix
+    nixfmt "$out"/.idx/dev.nix
+
+    ${if packageManager == "npm" then "( cd \$out && npm i --package-lock-only --ignore-scripts )" else ""}
+  '';
+}
